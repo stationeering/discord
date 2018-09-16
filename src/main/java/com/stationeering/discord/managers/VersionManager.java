bc3194e28263031a2ad9a38f7851e63642a3f55b
Melair <melair@caldonia.net>
2018-09-16 13:19:19 +0100

    Remove updates to prevent beta->public transitions.

--- src/main/java/com/stationeering/discord/managers/VersionManager.java
+++ src/main/java/com/stationeering/discord/managers/VersionManager.java
@@ -95,7 +95,6 @@ public class VersionManager implements Runnable {
             case "ping":
                 break;
             case "create":
-            case "update":
                 handleCreateUpdate(jsonObject);
                 break;
             default:
